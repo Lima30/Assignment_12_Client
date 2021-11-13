a33be2f58be539159fb8aa7f18647d3a95a8166e
Lima30 <limasultana111@gmail.comm>
2021-11-13 10:37:53 -0600

    server updated

--- src/Pages/ReviewShow/ReviewShow.js
+++ src/Pages/ReviewShow/ReviewShow.js
@@ -5,47 +5,20 @@ const ReviewShow = () => {
     const [review, setReview] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/review')
+        fetch('https://rocky-river-51306.herokuapp.com/review')
             .then(res => res.json())
             .then(data => setReview(data));
-        // .then(data => {
-        //     const slicedData = data.slice(0, 6)
-        //     setServices(slicedData)
-        // })
+
     }, []);
 
 
-    //     useEffect(() => {
-    //         fetch('http://localhost:5000/review')
-    //             .then(res => res.json())
-    //             .then(data => setReview(data));
-    //     })
-    // },[]);
 
 
     return (
         <div>
             <h2>CUSTOMER REVIEW</h2>
 
-            {/* <div id="review-container">
-                {review?.map((pd) => (
-                    <div className="review-container">
-                        <div className=" bg-info border border p-3 ">
 
-                            <h6 className="text-black">{pd?.user}</h6>
-
-                            <h3 className="text-black"> {pd?.description}</h3>
-
-
-
-
-                        </div>
-                    </div>
-                   
-
-                ))}
-
-            </div> */}
 
 
             <div className="services">
